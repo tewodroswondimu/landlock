7f5104aa0f3bf95bccf93fff80d25730b7fe199a
Tewodros Wondimu <tewodros.wr@gmail.com>
2017-11-29 20:47:07 +0200

    updated ui

--- landlock/Controllers/MarketPlaceViewController.swift
+++ landlock/Controllers/MarketPlaceViewController.swift
@@ -6,4 +6,45 @@
 //  Copyright © 2017 Tewodros Wondimu. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class MarketPlaceViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var marketPlaceTableView: UITableView!
+    
+    // Fake data to load into properties
+    let propertyNames = [""]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // Adds the line underneath the imageview
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cell.separatorInset = UIEdgeInsets.zero
+    }
+    
+    // Set the number of sections available in the table view
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    // Set the number of rows in the sections available in the table view
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    // Definition of each cell in the table view
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = marketPlaceTableView.dequeueReusableCell(withIdentifier: "MarketPlace", for: indexPath) as UITableViewCell
+        cell.textLabel?.text = "Property \(indexPath.row + 1)"
+        cell.detailTextLabel?.text = "Description \(indexPath.row + 1)"
+        return cell
+    }
+}
+
--- landlock/TabBarViewController.swift
+++ landlock/TabBarViewController.swift
@@ -9,17 +9,41 @@
 import UIKit
 
 class TabBarViewController: UITabBarController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // Set color of titles and icons in tabBar
+        self.tabBar.tintColor = self.hexStringToUIColor(hex: "74b4ae")
+        // Set color of background tabBar
+        // self.tabBar.barTintColor = UIColor.blue
+    }
+    
+    func hexStringToUIColor (hex:String) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        if ((cString.count) != 6) {
+            return UIColor.gray
+        }
+        
+        var rgbValue:UInt32 = 0
+        Scanner(string: cString).scanHexInt32(&rgbValue)
+        
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
