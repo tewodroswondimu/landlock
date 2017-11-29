3eda64bdd58ce6667625b0e9f66f550b5cd4afb9
Tewodros Wondimu <tewodros.wr@gmail.com>
2017-11-29 19:01:20 +0200

    Added UI for profile

--- landlock/Controllers/ProfileViewController.swift
+++ landlock/Controllers/ProfileViewController.swift
@@ -8,12 +8,15 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var profileRegistrationStatus: UIImageView!
     @IBOutlet weak var profilePictureView: UIImageView!
     
     @IBOutlet weak var profilePictureName: UILabel!
     @IBOutlet weak var profileAccountDetails: UILabel!
+    
+    @IBOutlet weak var userSettingsTableView: UITableView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,6 +34,34 @@ class ProfileViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    // Adds the line underneath the imageview
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cell.separatorInset = UIEdgeInsets.zero
+    }
+    
+    // Set the number of sections available in the table view
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    // Set the title of the different sections in the table view
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Settings"
+    }
+    
+    // Set the number of rows in the sections available in the table view
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    // Definition of each cell in the table view
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = userSettingsTableView.dequeueReusableCell(withIdentifier: "Setting", for: indexPath) as UITableViewCell
+        cell.textLabel?.text = "Transaction \(indexPath.row + 1)"
+        cell.detailTextLabel?.text = "Description \(indexPath.row + 1)"
+        return cell
+    }
     
 }
 
