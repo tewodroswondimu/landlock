9517b980d9cb31d8249000f5f0dd5e0e3e01f574
Tewodros Wondimu <tewodros.wr@gmail.com>
2017-11-29 20:46:26 +0200

    Changed tab bar tint, changed profile background, changed app name

---
+++ landlock/Controllers/MarketPlaceViewController.swift
@@ -0,0 +1,9 @@
+//
+//  marketPlaceViewController.swift
+//  landlock
+//
+//  Created by Tewodros Wondimu on 11/29/17.
+//  Copyright © 2017 Tewodros Wondimu. All rights reserved.
+//
+
+import Foundation
